4bed6b07ce3a5e186401faba85a80e285012974b
chu-98 <81007078+chu-98@users.noreply.github.com>
2022-06-20 21:58:00 +0900

    feat: what

--- src/components/chapter/lesson1/chapter1/unit/L1C1U1.jsx
+++ src/components/chapter/lesson1/chapter1/unit/L1C1U1.jsx
@@ -181,156 +181,157 @@ function L1C1U1() {
             </span>
           </div>
         </UnitDesc>
-        <OrangeHeader>
-          <div class="container px-4 mx-auto">
-            <div class="text-center">
-              <h1 class="font-extrabold font-heading text-yellow-100 md:text-2xl text-lg">
-                1-1. Messages (msg.rs)
-              </h1>
-            </div>
+      </Background>
+      <OrangeHeader>
+        <div class="container px-4 mx-auto">
+          <div class="text-center">
+            <h1 class="font-extrabold font-heading text-yellow-100 md:text-2xl text-lg">
+              1-1. Messages (msg.rs)
+            </h1>
           </div>
-        </OrangeHeader>
-        {msg?.map(e => (
-          <Content>
-            <div class="mx-auto flex flex-wrap justify-center border-dashed border-gray-200 border-b-2 py-16 bg-gray-700 px-8 md:px-4">
-              <ContentId>
-                <span class="text-center font-heading text-2xl text-black">
-                  {e.id}
-                </span>
-              </ContentId>
-              <div class="lg:w-1/2 w-full md:w-2/3">
-                <ContentTitle>
-                  <div class="flex sm:flex-nowrap">
-                    <div class="w-full lg:w-auto lg:pt-3 pt-2 pb-2 lg:pb-0">
-                      <h1 class="text-center md:text-left xl:text-2xl font-extrabold text-xl">
-                        {e.title}
-                      </h1>
-                    </div>
-                  </div>
-                </ContentTitle>
-
-                <ContentDesc>
-                  <span class="font-normal lg:text-base text-sm mb-4">
-                    {e.desc1}
-                  </span>
-                  <span class="font-normal lg:text-base text-sm mb-4">
-                    {e.desc2}
-                  </span>
-                  <span class="font-normal lg:text-base text-sm mb-4">
-                    {e.desc3}
-                  </span>
-                  <span class="font-normal lg:text-base text-sm mb-4">
-                    {e.desc4}
-                  </span>
-                  <span class="font-normal lg:text-base text-sm mb-4">
-                    {e.desc5}
-                  </span>
-                </ContentDesc>
-              </div>
-            </div>
-          </Content>
-        ))}
-        <GreenHeader>
-          <div class="container px-4 mx-auto">
-            <div class="text-center">
-              <h1 class="font-extrabold font-heading text-yellow-100 md:text-2xl text-lg">
-                1-2. Queries(query.rs)
-              </h1>
-            </div>
-          </div>
-        </GreenHeader>
-        {query?.map(e => (
-          <Content>
-            <div class="mx-auto flex flex-wrap justify-center border-dashed border-gray-200 border-b-2 py-16 bg-gray-700 px-8 md:px-4">
-              <ContentId>
-                <span class="text-center font-heading text-2xl text-black">
-                  {e.id}
-                </span>
-              </ContentId>
-              <div class="lg:w-1/2 w-full md:w-2/3">
-                <ContentTitle>
-                  <div class="flex sm:flex-nowrap">
-                    <div class="w-full lg:w-auto lg:pt-3 pt-2 pb-2 lg:pb-0">
-                      <h1 class="text-center md:text-left xl:text-2xl font-extrabold text-xl">
-                        {e.title}
-                      </h1>
-                    </div>
-                  </div>
-                </ContentTitle>
-
-                <ContentDesc>
-                  <span class="font-normal lg:text-base text-sm mb-4">
-                    {e.desc1}
-                  </span>
-                  <span class="font-normal lg:text-base text-sm mb-4">
-                    {e.desc2}
-                  </span>
-                  <span class="font-normal lg:text-base text-sm mb-4">
-                    {e.desc3}
-                  </span>
-                  <span class="font-normal lg:text-base text-sm mb-4">
-                    {e.desc4}
-                  </span>
-                  <span class="font-normal lg:text-base text-sm mb-4">
-                    {e.desc5}
-                  </span>
-                </ContentDesc>
-              </div>
-            </div>
-          </Content>
-        ))}
-        <BlueHeader>
-          <div class="container px-4 mx-auto">
-            <div class="text-center">
-              <h1 class="font-extrabold font-heading text-yellow-100 md:text-2xl text-lg">
-                1-2. Queries(query.rs)
-              </h1>
-            </div>
-          </div>
-        </BlueHeader>
+        </div>
+      </OrangeHeader>
+      {msg?.map(e => (
         <Content>
           <div class="mx-auto flex flex-wrap justify-center border-dashed border-gray-200 border-b-2 py-16 bg-gray-700 px-8 md:px-4">
-            <Check />
-            <Desc>
-              <ContentHeader>
+            <ContentId>
+              <span class="text-center font-heading text-2xl text-black">
+                {e.id}
+              </span>
+            </ContentId>
+            <div class="lg:w-1/2 w-full md:w-2/3">
+              <ContentTitle>
                 <div class="flex sm:flex-nowrap">
                   <div class="w-full lg:w-auto lg:pt-3 pt-2 pb-2 lg:pb-0">
                     <h1 class="text-center md:text-left xl:text-2xl font-extrabold text-xl">
-                      ReceiverNFT
+                      {e.title}
                     </h1>
                   </div>
                 </div>
-              </ContentHeader>
+              </ContentTitle>
+
               <ContentDesc>
-                <p class="font-normal lg:text-base text-sm mb-8">
-                  SendNft의 반대부에는 ReceiveNft가 있습니다. ReceiveNft는 CW721
-                  토큰을 다루고 싶은 모든 컨트랙트가 반드시 구현해야하는
-                  스펙입니다. (일반적으로 CW721 컨트랙트 자체가 구현하는 항목은
-                  아닙니다.)
-                </p>
-                <p class="font-normal lg:text-base text-sm mb-8">
-                  SendNft 메시지를 보내는 CW721 구현체 컨트랙트 주소는
-                  env.sender에서 다뤄지므로 위변조할 수 없습니다. ReceiveNft를
-                  다루는 컨트랙트에서는 이 발송인 주소가 토큰 컨트랙트 주소와
-                  일치하는지 확인하고 보장한 후 처리해야 합니다. 임의의 주소를
-                  모두 허용해서는 안 됩니다.
-                </p>
-                <p class="font-normal lg:text-base text-sm mb-8">
-                  sender는 토큰을 전송하고자 하는 원 주소입니다.
-                </p>
-                <p class="font-normal lg:text-base text-sm mb-8">
-                  token_id는 토큰의 고유한 ID입니다.
-                </p>
-                <p class="font-normal lg:text-base text-sm mb-8">
-                  msg는 컨트랙트-맞춤 메시지 혹은 임의 데이터로 디코딩 될 수
-                  있는 바이너리입니다. 만일 기본 액션만 수행하고자 한다면 msg는
-                  비어있을 수 있습니다.
-                </p>
+                <span class="font-normal lg:text-base text-sm mb-4">
+                  {e.desc1}
+                </span>
+                <span class="font-normal lg:text-base text-sm mb-4">
+                  {e.desc2}
+                </span>
+                <span class="font-normal lg:text-base text-sm mb-4">
+                  {e.desc3}
+                </span>
+                <span class="font-normal lg:text-base text-sm mb-4">
+                  {e.desc4}
+                </span>
+                <span class="font-normal lg:text-base text-sm mb-4">
+                  {e.desc5}
+                </span>
               </ContentDesc>
-            </Desc>
+            </div>
           </div>
         </Content>
-      </Background>
+      ))}
+      <GreenHeader>
+        <div class="container px-4 mx-auto">
+          <div class="text-center">
+            <h1 class="font-extrabold font-heading text-yellow-100 md:text-2xl text-lg">
+              1-2. Queries(query.rs)
+            </h1>
+          </div>
+        </div>
+      </GreenHeader>
+      {query?.map(e => (
+        <Content>
+          <div class="mx-auto flex flex-wrap justify-center border-dashed border-gray-200 border-b-2 py-16 bg-gray-700 px-8 md:px-4">
+            <ContentId>
+              <span class="text-center font-heading text-2xl text-black">
+                {e.id}
+              </span>
+            </ContentId>
+            <div class="lg:w-1/2 w-full md:w-2/3">
+              <ContentTitle>
+                <div class="flex sm:flex-nowrap">
+                  <div class="w-full lg:w-auto lg:pt-3 pt-2 pb-2 lg:pb-0">
+                    <h1 class="text-center md:text-left xl:text-2xl font-extrabold text-xl">
+                      {e.title}
+                    </h1>
+                  </div>
+                </div>
+              </ContentTitle>
+
+              <ContentDesc>
+                <span class="font-normal lg:text-base text-sm mb-4">
+                  {e.desc1}
+                </span>
+                <span class="font-normal lg:text-base text-sm mb-4">
+                  {e.desc2}
+                </span>
+                <span class="font-normal lg:text-base text-sm mb-4">
+                  {e.desc3}
+                </span>
+                <span class="font-normal lg:text-base text-sm mb-4">
+                  {e.desc4}
+                </span>
+                <span class="font-normal lg:text-base text-sm mb-4">
+                  {e.desc5}
+                </span>
+              </ContentDesc>
+            </div>
+          </div>
+        </Content>
+      ))}
+      <BlueHeader>
+        <div class="container px-4 mx-auto">
+          <div class="text-center">
+            <h1 class="font-extrabold font-heading text-yellow-100 md:text-2xl text-lg">
+              1-2. Queries(query.rs)
+            </h1>
+          </div>
+        </div>
+      </BlueHeader>
+      <Content>
+        <div class="mx-auto flex flex-wrap justify-center border-dashed border-gray-200 border-b-2 py-16 bg-gray-700 px-8 md:px-4">
+          <Check />
+          <Desc>
+            <ContentHeader>
+              <div class="flex sm:flex-nowrap">
+                <div class="w-full lg:w-auto lg:pt-3 pt-2 pb-2 lg:pb-0">
+                  <h1 class="text-center md:text-left xl:text-2xl font-extrabold text-xl">
+                    ReceiverNFT
+                  </h1>
+                </div>
+              </div>
+            </ContentHeader>
+            <ContentDesc>
+              <p class="font-normal lg:text-base text-sm mb-8">
+                SendNft의 반대부에는 ReceiveNft가 있습니다. ReceiveNft는 CW721
+                토큰을 다루고 싶은 모든 컨트랙트가 반드시 구현해야하는
+                스펙입니다. (일반적으로 CW721 컨트랙트 자체가 구현하는 항목은
+                아닙니다.)
+              </p>
+              <p class="font-normal lg:text-base text-sm mb-8">
+                SendNft 메시지를 보내는 CW721 구현체 컨트랙트 주소는
+                env.sender에서 다뤄지므로 위변조할 수 없습니다. ReceiveNft를
+                다루는 컨트랙트에서는 이 발송인 주소가 토큰 컨트랙트 주소와
+                일치하는지 확인하고 보장한 후 처리해야 합니다. 임의의 주소를
+                모두 허용해서는 안 됩니다.
+              </p>
+              <p class="font-normal lg:text-base text-sm mb-8">
+                sender는 토큰을 전송하고자 하는 원 주소입니다.
+              </p>
+              <p class="font-normal lg:text-base text-sm mb-8">
+                token_id는 토큰의 고유한 ID입니다.
+              </p>
+              <p class="font-normal lg:text-base text-sm mb-8">
+                msg는 컨트랙트-맞춤 메시지 혹은 임의 데이터로 디코딩 될 수 있는
+                바이너리입니다. 만일 기본 액션만 수행하고자 한다면 msg는
+                비어있을 수 있습니다.
+              </p>
+            </ContentDesc>
+          </Desc>
+        </div>
+      </Content>
+
       <Navigator title="Welcome to spaceship factory" />
       <Footer />
     </>
